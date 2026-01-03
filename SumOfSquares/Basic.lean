7847ccf567c31414400962f6ccb141e4bf318dc6
Dave King <tildedave@gmail.com>
2026-01-03 04:10:32 -0500

    Descent lemma proven

--- SumOfSquares/Basic.lean
+++ SumOfSquares/Basic.lean
@@ -7,7 +7,7 @@ import Mathlib.Util.Delaborators
 #check Nat.Coprime
 
 def SumOfSquares (a : ℤ) : Prop
-  := ∃ p > 0, ∃ q > 0, p.gcd q = 1 ∧ p^2 + q^2 = a
+  := ∃ p q, p.gcd q = 1 ∧ p^2 + q^2 = a
 
 example {a : ℕ} : (a = 1) ∨ (a ≠ 1) := by exact eq_or_ne a 1
 
@@ -16,16 +16,15 @@ example {a : ℕ} : (a = 1) ∨ (a ≠ 1) := by exact eq_or_ne a 1
 -- q = x^2 + y^2 is a prime divisor of N. Then N/q is also a sum of two
 -- relatively prime squares.
 
-
 -- does not force coprime but this is a lemma
 def PrimeSumOfSquares (q : ℕ) : Prop
-  := Nat.Prime q ∧ (∃ x > 0, ∃ y > 0, x ^ 2 + y ^ 2 = (q : ℤ))
+  := Nat.Prime q ∧ (∃ x y, x ^ 2 + y ^ 2 = (q : ℤ))
 
--- x and y have to be coprime or else q has a nontrivial divisor.
 lemma prime_sumofsquares_coprime {q : ℕ} {x y : ℤ}
-  (qprime : Nat.Prime q) (hx : x > 0) (hy : y > 0)
+  (qprime : Nat.Prime q)
   (hq : x ^ 2 + y ^ 2 = (q : ℤ))
   : x.gcd y = 1 := by
+    have q_bound : (1 < q) := by exact Nat.Prime.one_lt qprime
     rcases eq_or_ne (x.gcd y) 1 with h | impossible
     · exact h
     · exfalso
@@ -41,7 +40,30 @@ lemma prime_sumofsquares_coprime {q : ℕ} {x y : ℤ}
       rw [Nat.dvd_prime qprime] at this
       rcases this with eq_1 | eq_p
       · tauto
-      · nlinarith [Int.gcd_le_left y hx]
+      · -- x and y having q as their common divisor means that it looks
+        -- something like (qf)^2 + (qg)^2 = q which is obviously silly.
+        have q_div_x : ↑q ∣ x := by
+          rw [<- eq_p]
+          exact Int.gcd_dvd_left x y
+        have q_div_y : ↑q ∣ y := by
+          rw [<- eq_p]
+          exact Int.gcd_dvd_right x y
+        obtain ⟨f, hf⟩ := q_div_x
+        obtain ⟨g, hg⟩ := q_div_y
+        rw [
+          show ↑q = (↑q) * (1 : ℤ) by ring,
+          hf,
+          hg,
+          show (↑q * f)^2 + (↑q * g)^2 = ↑q * (↑q * f * f + ↑q * g * g) by ring
+        ] at hq
+        rw [Int.mul_eq_mul_left_iff (by omega)] at hq
+        have : ↑q ∣ (1 : ℤ) := by
+          use (f * f + g * g)
+          calc (1 : ℤ) = ↑q * f * f + ↑q * g * g := hq.symm
+               _ = ↑q * (f * f + g * g) := by ring
+        have : q ∣ 1 := Int.ofNat_dvd.mp this
+        rw [Nat.dvd_one] at this
+        omega
 
 lemma relprime_lemma {c d x y : ℤ} (h : (c * x - d * y).gcd (d * x + c * y) = 1)
   : c.gcd d = 1 := by
@@ -54,21 +76,48 @@ lemma relprime_lemma {c d x y : ℤ} (h : (c * x - d * y).gcd (d * x + c * y) = 
     (Int.dvd_sub (Int.dvd_mul_of_dvd_left qdivc) (Int.dvd_mul_of_dvd_left qdivd))
     (Int.dvd_add (Int.dvd_mul_of_dvd_left qdivd) (Int.dvd_mul_of_dvd_left qdivc))
 
--- example {x y q : ℤ} (hq : q ≠ 0) (h : x = y) : (x * q = y * q) := by exact
---   (Int.mul_eq_mul_right_iff hq).mpr h
+lemma natabs_sq {y : ℤ} {n : ℕ} (h : y.natAbs = n) : y^2 = n * n := by
+  grind
+
+lemma eq_mult_self_implies_one {a : ℤ} (ha : a ≠ 0) (h : a * a = a) : a = 1 := by
+  rw [show a = a * 1 by ring, show a * 1 * (a * 1) = a * a by ring] at h
+  rw [Int.mul_eq_mul_left_iff ha] at h
+  exact h
+
+lemma prime_sum_of_squares_neq_zero_left {x y : ℤ} {p : ℕ} (pp : Nat.Prime p)
+  (h : x ^ 2 + y ^ 2 = p) : x ≠ 0 := by
+  intro hx
+  rw [hx] at h
+  ring_nf at h
+  have : y ∣ ↑p := by
+    use y
+    linarith
+  have : y.natAbs ∣ p := by exact Int.ofNat_dvd_right.mp this
+  apply (Nat.Prime.eq_one_or_self_of_dvd pp) at this
+  rcases this with left | right
+  · apply Nat.Prime.ne_one pp
+    apply natabs_sq at left; rw [left] at h
+    grind
+  · apply Nat.Prime.ne_one pp
+    apply natabs_sq at right; rw [right] at h
+    apply eq_mult_self_implies_one (Int.ofNat_ne_zero.mpr (Nat.Prime.ne_zero pp)) at h
+    rw [<- Int.ofNat_inj]
+    exact h
 
 lemma sum_of_squares_descent (N : ℤ) (h : SumOfSquares N) (q : ℕ)
   (hq : PrimeSumOfSquares q ∧ ↑q ∣ N) :
   (SumOfSquares (N / q)) := by
-  obtain ⟨a, a_bound, b, b_bound, ab_coprime, hab⟩ := h
+  obtain ⟨a, b, ab_coprime, hab⟩ := h
   have qprime : (Nat.Prime q) := by
     obtain ⟨⟨qprime, _⟩, _⟩ := hq
     exact qprime
   -- therefore we should have q divides (xb - ay) (xb + ay)
   -- the wlog argument in the book relies on the ability to switch x and y
-  -- around which we can't do if we've already extracted them.
-  have exists_d : ∃x > 0, ∃y > 0, ∃d > 0, x^2 + y^2 = (q : ℤ) ∧ q * d = x * b - a * y := by
-    obtain ⟨⟨_, x, x_bound, y, y_bound, hxy⟩, qdiv⟩ := hq
+  -- around which we can't do if we've already extracted them.  so we need to
+  -- extract them all together.
+  have exists_d : ∃x y d, x^2 + y^2 = (q : ℤ) ∧ q * d = x * b - a * y := by
+    obtain ⟨⟨_, x, y, hxy⟩, qdiv⟩ := hq
+    have xy_coprime : x.gcd y = 1 := prime_sumofsquares_coprime qprime hxy
     have q_div : ↑q ∣ (x * b - a * y) * (x * b + a * y) := by
       rw [show (x * b - a * y) * (x * b + a * y) = x^2 * N - a^2 * q by
         calc (x * b - a * y) * (x * b + a * y) = x^2 * b^2 - a^2 * y^2 := by ring
@@ -78,10 +127,15 @@ lemma sum_of_squares_descent (N : ℤ) (h : SumOfSquares N) (q : ℕ)
     have q_div_or : ↑q ∣ x * b - a * y ∨ ↑q ∣ x * b + a * y := by
       exact Int.Prime.dvd_mul' qprime q_div
     -- now we can do cases based on negative/positive so that we can find that
-    -- positive d, and we can switch x/y/neg x,neg y around etc.
-    sorry
-  obtain ⟨x, x_bound, y, y_bound, d, d_bound, hxy, hd⟩ := exists_d
-  have xy_coprime : x.gcd y = 1 := prime_sumofsquares_coprime qprime x_bound y_bound hxy
+    -- q ∣ xb — ay
+    rcases q_div_or with left | right
+    · obtain ⟨d, hd⟩ := by exact left
+      refine ⟨x, y, d, by tauto⟩
+    · obtain ⟨d, hd⟩ := by exact right
+      refine ⟨x, -y, d, ?_⟩
+      constructor <;> grind
+  obtain ⟨x, y, d, hxy, hd⟩ := exists_d
+  have xy_coprime : x.gcd y = 1 := prime_sumofsquares_coprime qprime hxy
   have x_div_a_plus_dy_rel : (a + d * y) * y = x * (b - d * x) := by
     calc (a + d * y) * y = a * y + d * y^2 := by ring
               _              = x * b - d * q + d * y^2 := by linarith [hd]
@@ -90,22 +144,22 @@ lemma sum_of_squares_descent (N : ℤ) (h : SumOfSquares N) (q : ℕ)
   have claim : x ∣ (a + d * y) := by
     refine Int.dvd_of_dvd_mul_left_of_gcd_one ?_ xy_coprime
     use (b - d * x)
-  have : ∃c > 0, a + d * y = c * x := by
+  have : ∃c, a + d * y = c * x := by
     obtain ⟨c', hc⟩ := claim
-    rcases (Int.lt_trichotomy c' 0) with lt | impossible | gt
-    · nlinarith
-    · nlinarith
-    · exact ⟨c', by omega, by linarith [hc]⟩
-  obtain ⟨c, c_bound, hc⟩ := this
+    refine ⟨c', ?_⟩
+    linarith [hc]
+  obtain ⟨c, hc⟩ := this
   have hd : b = d * x + c * y := by
     -- this is supposed to follow by from cxy = (a + dy)y = xb - dx^2
     -- I don't really understand why however.
     -- OK so cxy = xb - dx^2 -> cy = b - dx.  Easy enough
+    have x_neq_0 : x ≠ 0 := by
+      apply prime_sum_of_squares_neq_zero_left qprime hxy
     rw [hc,
         show c * x * y = x * (c * y) by ring,
-        Int.mul_eq_mul_left_iff (by omega)] at x_div_a_plus_dy_rel
+        Int.mul_eq_mul_left_iff x_neq_0] at x_div_a_plus_dy_rel
     linarith [x_div_a_plus_dy_rel]
-  refine ⟨c, by omega, d, by omega, ?_, ?_⟩
+  refine ⟨c, d, ?_, ?_⟩
   · rw [hd] at ab_coprime
     -- a + d * y = c * x
     rw [show a = c * x - d * y by omega] at ab_coprime
@@ -115,5 +169,5 @@ lemma sum_of_squares_descent (N : ℤ) (h : SumOfSquares N) (q : ℕ)
       apply Nat.Prime.ne_zero qprime
     · calc ↑q * (c ^ 2 + d ^ 2) = (x^2  + y^2) * (c^2 + d^2) := by rw [hxy]
             _ = (c * x - d * y)^2 + (d * x + c * y)^2 := by ring
-            _ = a^2 + b^2 := by nlinarith [hc, hd]
+            _ = a^2 + b^2 := by grind
       exact hab
